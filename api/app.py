44935ed75a01c4902291f580777f6321bc872917
Pankaj kumar shukla <89392725+shuklapankaj36@users.noreply.github.com>
2025-04-08 08:32:18 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,1 +1,14 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from model.run_inference import answer_question
+
+app = FastAPI()
+
+class QuestionRequest(BaseModel):
+    question: str
+
+@app.post("/ask")
+def get_answer(request: QuestionRequest):
+    response = answer_question(request.question)
+    return {"answer": response}
 
